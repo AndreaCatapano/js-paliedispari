cc15865df56175660cecc196ad0457b4ccce3c41
AndreaCatapano <a.catapano96@gmail.com>
2025-02-28 16:13:39 +0100

    feat: added a number of try for user

--- Dispari /main.js
+++ Dispari /main.js
@@ -1,44 +1,52 @@
 let userChoiceInput;
 let userNumberInput;
 let isInputValid;
+let usertry = 0;
 
 do {
-    userChoiceInput = (prompt(`Scegli tra:"Pari" o "Dispari"`)).toLowerCase();
-    const isPari = userChoiceInput === "pari";
-    const isDispari = userChoiceInput === "dispari";
+    if (usertry <= 10) {
+        userChoiceInput = (prompt(`Scegli tra:"Pari" o "Dispari"`)).toLowerCase();
+        const isPari = userChoiceInput === "pari";
+        const isDispari = userChoiceInput === "dispari";
 
-    if (isPari || isDispari) {
-        userNumberInput = parseInt(prompt("Scrivi un numero da 1 a 5!"));
-        const isANumber = !isNaN(userNumberInput);
-        const isValidNumber = userNumberInput >= 1 && userNumberInput <= 5;
-        if (isANumber && isValidNumber) {
-            const randomNumber = randomNumber5();
-            console.log(randomNumber)
-            let result = userNumberInput + randomNumber;
-            let isEven = checkEvenNumber(result);
+        if (isPari || isDispari) {
+            userNumberInput = parseInt(prompt("Scrivi un numero da 1 a 5!"));
+            const isANumber = !isNaN(userNumberInput);
+            const isValidNumber = userNumberInput >= 1 && userNumberInput <= 5;
+            if (isANumber && isValidNumber) {
+                const randomNumber = randomNumber5();
+                let result = userNumberInput + randomNumber;
+                let isEven = checkEvenNumber(result);
 
-            if ((isEven && isPari) || (!isEven && isDispari)){
-                console.log("Hai indovinato")
+                if ((isEven && isPari) || (!isEven && isDispari)) {
+                    console.log("Hai indovinato");
+                } else {
+                    console.log("Hai sbagliato");
+                }
+
+                isInputValid = true;
+                usertry = 0;
             } else {
-                console.log("Hai sbagliato")
+                alert("Inserisci un numero da 1 a 5!");
+                isInputValid = false;
+                usertry++;
             }
- 
-            isInputValid = true;
         } else {
-            alert("Inserisci un numero da 1 a 5!");
+            alert(`Inserisci una scelta valida tra: "Pari" o "Dispari"`);
             isInputValid = false;
-        } 
+            usertry++;
+        }
     } else {
-        alert(`Inserisci una scelta valida tra: "Pari" o "Dispari"`);
-        isInputValid = false;
+        isInputValid = true; 
+        alert("Tentativi finiti, aggiorna la pagina per riprovare")
     }
 } while (!isInputValid)
 
 
-function randomNumber5 (){
+function randomNumber5() {
     return Math.floor(Math.random() * 5) + 1;
 }
 
-function checkEvenNumber (result) {
-    return result % 2 === 0; 
+function checkEvenNumber(result) {
+    return result % 2 === 0;
 }
